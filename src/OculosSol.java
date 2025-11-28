21db1740918ef0b91a917376d0f19982950a4bd7
Lightlaw33 <luizfeberaldo@gmail.com>
2025-11-28 10:45:56 -0300

    Adicionado gets e setters oculosSol e produtos

--- src/OculosSol.java
+++ src/OculosSol.java
@@ -1,4 +1,21 @@
 
-public class OculosSol {
+public class OculosSol extends Produtos {
 
+	String corLente;
+	
+	public OculosSol(String cor, float preco, String pigmentacao){
+		super(cor, preco);
+		
+		this.corLente = pigmentacao;
+	}
+	
+	public void setCorLente(String pigmento){
+		this.corLente = pigmento;
+	}
+	
+	public String getCorLente(){
+		return corLente;
+	}
+	
+	
 }
--- src/Produtos.java
+++ src/Produtos.java
@@ -1,4 +1,27 @@
 
 public class Produtos {
 
+		String cor;
+		float preco;
+		
+		public Produtos(String c, float p){
+			this.cor = c;
+			this.preco = p;
+		}
+		
+		public String getCor(){
+			return cor;
+		}
+		
+		public float getPreco(){
+			return preco;
+		}
+		
+		public void setCor(String color){
+			this.cor = color;
+		}
+		
+		public void setPreco(float price){
+			this.preco = price;
+		}
 }
